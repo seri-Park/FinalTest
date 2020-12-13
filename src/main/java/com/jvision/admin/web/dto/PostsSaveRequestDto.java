709aba9313f1512284df770362c71a6f30bbf145
Seri-Park <qkrtpfl1016@naver.com>
2020-12-14 03:26:07 +0900

    hello

---
+++ src/main/java/com/jvision/admin/web/dto/PostsSaveRequestDto.java
@@ -0,0 +1,34 @@
+package com.jvision.admin.web.dto;
+
+import com.jvision.admin.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+
+    private String id;
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public PostsSaveRequestDto(String id, String title, String content, String author)
+    {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Posts toEntity()
+    {
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
